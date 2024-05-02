33c91fdf4701836e6639f7c9504a4013b101a045
blend <151626101+blendtwenty@users.noreply.github.com>
2024-05-02 16:16:01 +1000

    nodeResolution to run in "production" mode

--- dev/configs/web-test-runner.config.mjs
+++ dev/configs/web-test-runner.config.mjs
@@ -1,11 +1,13 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default {
-  nodeResolve: true,
   rootDir: '../src/',
   files: ['../src/components/**/*.test.*'],
   concurrentBrowsers: 1,
   concurrency: 2,
+  nodeResolve: {
+    exportConditions: ['production', 'default']
+  },
   testFramework: {
     config: {
       timeout: 3000,

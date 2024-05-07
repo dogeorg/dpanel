d33fe83c8a01405004f3185b0bb27ec8cf2b7a17
blend <151626101+blendtwenty@users.noreply.github.com>
2024-05-07 16:42:57 +1000

    tidy app.js

--- src/app.js
+++ src/app.js
@@ -1,56 +1,50 @@
-import { LitElement, html, css, nothing } from '/vendor/@lit/all@3.1.2/lit-all.min.js';
+import {
+  LitElement,
+  html,
+  nothing,
+} from "/vendor/@lit/all@3.1.2/lit-all.min.js";
 
 // Add shoelace once. Use components anywhere.
-import { setBasePath } from '/vendor/@shoelace/cdn@2.14.0/utilities/base-path.js';
-import '/vendor/@shoelace/cdn@2.14.0/shoelace.js';
+import { setBasePath } from "/vendor/@shoelace/cdn@2.14.0/utilities/base-path.js";
+import "/vendor/@shoelace/cdn@2.14.0/shoelace.js";
 
 // Import stylesheets
-import {
-  mainStyles,
-  navStyles,
-  utilStyles
-} from '/styles/app.index.styles.js';
+import { mainStyles, navStyles, utilStyles } from "/styles/app.index.styles.js";
 
 // App state (singleton)
-import { store } from '/state/store.js';
-import { StoreSubscriber } from '/state/subscribe.js';
-
-// Layouts
-import '/components/layouts/default-layout.js';
+import { store } from "/state/store.js";
+import { StoreSubscriber } from "/state/subscribe.js";
 
 // Views
-import '/components/views/index.js';
-import '/components/views/welcome-dialog/index.js';
+import "/components/views/index.js";
+import "/components/views/welcome-dialog/index.js";
 
 // Components
-import '/utils/debug-panel.js'; 
+import "/utils/debug-panel.js";
 
 // Router (singleton)
-import { getRouter } from '/router/router.js'
+import { getRouter } from "/router/router.js";
 
 // Utils
-import debounce from '/utils/debounce.js';
+import debounce from "/utils/debounce.js";
 
 // Do this once to set the location of shoelace assets (icons etc..)
-setBasePath('/vendor/@shoelace/cdn@2.14.0/');
+setBasePath("/vendor/@shoelace/cdn@2.14.0/");
 
 // Main web socket channel (singleton)
-import { mainChannel } from '/controllers/sockets/main-channel.js'
+import { mainChannel } from "/controllers/sockets/main-channel.js";
 
 class DPanelApp extends LitElement {
-
   static properties = {
     menuVisible: { type: Boolean },
     currentPath: { type: String },
-    routerState: { type: Object }
-  }
+  };
 
   constructor() {
     super();
     this.context = new StoreSubscriber(this, store);
     this.menuVisible = false;
-    this.currentPath = '';
-    this.routerState = null;
+    this.currentPath = "";
     this._debouncedHandleResize = debounce(this._handleResize.bind(this), 50);
     this.mainChannel = mainChannel;
   }
@@ -58,55 +52,35 @@ class DPanelApp extends LitElement {
   connectedCallback() {
     super.connectedCallback();
 
-    // Fetch main manifest (for pup listing)
-    // this.fetchManifestData();
-
     // Add the resize event listener
-    window.addEventListener('resize', this._debouncedHandleResize);
+    window.addEventListener("resize", this._debouncedHandleResize);
     // Initial check to set orientation on load
     this._handleResize();
 
     // Instanciate a web socket connection and add app as an observer
     this.mainChannel.addObserver(this);
-
   }
 
   disconnectedCallback() {
-    window.removeEventListener('resize', this._debouncedHandleResize);
+    window.removeEventListener("resize", this._debouncedHandleResize);
     this.mainChannel.removeObserver(this);
     super.disconnectedCallback();
   }
 
   firstUpdated() {
     // Initialise our router singleton and provide it a target elemenet.
-    const router = getRouter(this.shadowRoot.querySelector('#Outlet'));
-
-    // For demonstration
-    const alertExample = this.shadowRoot.querySelector('#AlertExample');
-    setTimeout(() => {
-      //  alertExample && alertExample.show();
-    }, 3000);
+    getRouter(this.shadowRoot.querySelector("#Outlet"));
   }
 
   _handleResize() {
     // Determine the orientation based on the window width
-    const orientation = window.innerWidth > 920 ? 'landscape' : 'portrait';
+    const orientation = window.innerWidth > 920 ? "landscape" : "portrait";
     // Update the appContext.orientation state
-    store.updateState({ appContext: { orientation }});
+    store.updateState({ appContext: { orientation } });
   }
 
-  // async fetchManifestData() {
-  //   try {
-  //     const manifestData = await getManifests();
-  //     this.manifest = manifestData;
-  //     this.requestUpdate();
-  //   } catch (error) {
-  //     console.error('Error fetching manifest:', error);
-  //   }
-  // }
-
   openDrawer() {
-    const drawer = this.shadowRoot.querySelector('sl-drawer');
+    const drawer = this.shadowRoot.querySelector("sl-drawer");
     drawer.show();
   }
 
@@ -114,26 +88,14 @@ class DPanelApp extends LitElement {
     this.menuVisible = !this.menuVisible;
   }
 
-  renderList() {
-    if (!this.manifest) return;
-    return html`
-      ${Object.entries(this.manifest).map(([key, value]) => html`
-        <sl-menu-item>
-          <sl-icon slot="prefix" name="${value.icon}"></sl-icon>
-          <a href="${value.path}">${value.name}</a>
-        </sl-menu-item>
-      `)}
-    `;
-  }
-
   render() {
-    const CURPATH = this.context.store.appContext.pathname || ''
-    const hideNav = !CURPATH.startsWith('/login');
+    const CURPATH = this.context.store.appContext.pathname || "";
+    const hideNav = !CURPATH.startsWith("/login");
 
     return html`
       <div id="App">
-
-        ${hideNav ? html `
+        ${hideNav
+          ? html`
           <nav id="Nav">
             <div id="GutterNav" ?open=${this.menuVisible}>
               <div id="logo" class="gutter-menu-item" @click=${() => window.location.reload(true)}>
@@ -147,22 +109,22 @@ class DPanelApp extends LitElement {
             <div id="Side" ?open=${this.menuVisible}>
               <div class"nav-body">
                 <div class="menu-label">dpanel v0.0.2</div>
-                <div class="menu-item ${CURPATH === '/' ? 'active' : ''}">
+                <div class="menu-item ${CURPATH === "/" ? "active" : ""}">
                   <sl-icon name="house-heart-fill"></sl-icon>
                   <a href="/">Such Home</a>
                 </div>
 
-                <div class="menu-item ${CURPATH.startsWith('/pups') ? 'active' : ''}">
+                <div class="menu-item ${CURPATH.startsWith("/pups") ? "active" : ""}">
                   <sl-icon name="box-seam"></sl-icon>
                   <a href="/pups">Much Pups</a>
                 </div>
 
-                <div class="menu-item ${CURPATH.startsWith('/stats') ? 'active' : ''}">
+                <div class="menu-item ${CURPATH.startsWith("/stats") ? "active" : ""}">
                   <sl-icon name="heart-pulse-fill"></sl-icon>
                   <a href="/stats">Very Stats</a>
                 </div>
 
-                <div class="menu-item ${CURPATH.startsWith('/config') ? 'active' : ''}">
+                <div class="menu-item ${CURPATH.startsWith("/config") ? "active" : ""}">
                   <sl-icon name="sliders"></sl-icon>
                   <a href="/config">So Config</a>
                 </div>
@@ -177,10 +139,11 @@ class DPanelApp extends LitElement {
               </div>
             </div>
           </nav>
-        ` : nothing }
-          <main id="Main">
-            <div id="Outlet"></div>
-          </main>
+        `
+          : nothing}
+        <main id="Main">
+          <div id="Outlet"></div>
+        </main>
       </div>
 
       <aside>
@@ -188,11 +151,13 @@ class DPanelApp extends LitElement {
         <debug-panel></debug-panel>
       </aside>
 
-      <style>${mainStyles}</style>
-      <style>${navStyles}</style>
-      <style>${utilStyles}</style>
+      <style>
+        ${mainStyles}
+        ${navStyles}
+        ${utilStyles}
+      </style>
     `;
   }
 }
 
-customElements.define('dpanel-app', DPanelApp);
+customElements.define("dpanel-app", DPanelApp);
